21b5a4a27669db58da1f34f988948459d41ba31a
Jonathan <jonathan.hartmayer@gmail.com>
2019-01-10 21:30:05 -0800

    Add Map Class

--- W1D5.rb
+++ W1D5.rb
@@ -37,3 +37,33 @@ class Queue
         @array.first
     end
 end
+
+class Map
+    attr_reader :array
+
+    def initialize
+        @array = []
+    end
+
+    def set(key, value)
+        matched_key = @array.select {|el| el[0] == key}
+        if @array.select {|el| el[0] == key}.empty?
+            @array.push([key, value])
+        else
+            @array[@array.index(matched_key)] = [key, value]
+        end
+    end
+
+    def get(key)
+        @array.select {|el| el[0] == key}[1]
+    end
+
+    def delete(key)
+        matched_key = @array.select {|el| el[0] == key}
+        @array.delete_at(@array.index(matched_key))
+    end
+
+    def show
+        @array
+    end
+end
