c351b3b473fd942663d4d5987d4fb32c807defd7
Jonathan <jonathan.hartmayer@gmail.com>
2019-01-10 21:09:42 -0800

    Add Queue Class

--- W1D5.rb
+++ W1D5.rb
@@ -17,3 +17,23 @@ class Stack
         @array.last
     end
 end
+
+class Queue
+    attr_reader :array
+
+    def initialize
+        @array = []
+    end
+
+    def enqueue(el)
+        @array.push(el)
+    end
+
+    def dequeue
+        @array.shift
+    end
+
+    def peek 
+        @array.first
+    end
+end
