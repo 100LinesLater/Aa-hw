066cc7085e3f16b94f3fc26d3c5ea701afe5b0e1
Jonathan <jonathan.hartmayer@gmail.com>
2019-01-12 15:51:02 -0800

    Finish  #show_sequence, #require_sequence, and #reset_game in Simon class

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -10,7 +10,13 @@ class Simon
   end
 
   def play
-
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
+    print "New Game? (y/n)   "
+    play if gets.chomp == "y"
   end
 
   def take_turn
@@ -18,31 +24,42 @@ class Simon
     require_sequence
     unless game_over
       round_success_message 
-      sequence_length += 1
+      @sequence_length += 1
     end
   end
 
   def show_sequence
-
+    add_random_color
+    counter = sequence_length
+    until counter == 0
+      print "#{seq[counter - 1]} "
+      sleep(1.5)
+      system "clear"
+      counter -= 1
+    end
   end
 
   def require_sequence
-
+    print "Return sequence (ex: green, yellow, blue): "
+    guess = gets.chomp.split(", ")
+    @game_over = true unless seq == guess
   end
 
   def add_random_color
-    seq << COLORS.sample
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "Good job! Next round..."
   end
 
   def game_over_message
-
+    puts "Awww so close."
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false 
+    @seq = []
   end
 end
